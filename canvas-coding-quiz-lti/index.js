8d6655493944240f5516cb72eecf814b5c34dc80
Kyle Graehl <kyle@graehlarts.com>
2017-09-20 09:08:25 -0700

    working canvas outcomes

---
+++ canvas-coding-quiz-lti/index.js
@@ -0,0 +1,133 @@
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const lti = require('ims-lti')
+const ejs = require('ejs')
+const fetch = require('node-fetch')
+const Remarkable = require('remarkable')
+const ropts = {typographer: true,html:true,linkify:true,linkTarget:"_blank"}
+const md = new Remarkable(ropts)
+const fs = require('fs')
+
+const cheapsession = {}
+
+const fcc_includes = [ 'seed/challenges/02-javascript-algorithms-and-data-structures/basic-javascript.json' ]
+const fcc_data = JSON.parse(fs.readFileSync(fcc_includes[0]))
+const fcc_index = {}
+for (let challenge of fcc_data.challenges) {
+  fcc_index[challenge.id] = challenge
+}
+
+
+function getAssignment(id) {
+  if (fcc_index[id]) {
+    var challenge = fcc_index[id]
+    console.log('found FCC challenge',challenge)
+    return challenge
+  }
+}
+
+app.use(express.static('public'))
+app.use(bodyParser.urlencoded());
+app.enable('trust proxy') // this lets req.proto == 'https'
+const consumer_key = 'consumerkey'
+const consumer_secret = 'consumersecret'
+
+const canvas_base = 'http://canvas-dev.tlmworks.org'
+// created on site admin under /profile
+const canvas_api_token = 'PDbk49hfZsQ7CzkCQ6Y9IuBmOXQSfvaTjjQFhYqLVG5Qgcpz0KXiISn2J9Z2rA9G'
+
+async function testCanvasAPI() {
+  const headers = {'Authorization':`Bearer ${canvas_api_token}`}
+  const result = await fetch(`${canvas_base}/api/v1/courses`, {headers})
+  const j = await result.json()
+  console.log('API /courses',j)
+}
+
+app.get('/gitlab/:org/:repo/:page', async (req, res) => {
+  const url = `https://gitlab.tlmworks.org/${req.params.org}/${req.params.repo}/wikis/${req.params.page}.md`
+  console.log('get url',url)
+  const resp = await fetch(url)
+  const text = await resp.text()
+  const html = md.render(text)
+  res.send(html)
+})
+
+app.post('/lti-grade', bodyParser.json(), async (req, res) => {
+  console.log('submit solution params',req.body)
+  var sessid = req.body.session
+  var data = cheapsession[sessid]
+
+  
+  if (data) {
+    console.log('found session',data)
+    var origbody = data.req.body
+    // use these two to check if the student already made a submission
+    // get single user's submission:
+    // https://canvas.instructure.com/doc/api/submissions.html#method.submissions_api.show
+    var canvas_assignment_id = origbody.custom_canvas_assignment_id
+    var canvas_course_id = origbody.custom_canvas_course_id
+    var provider = data.provider
+    var assignment = data.assignment
+    var code = req.body.code
+    var correct = req.body.state &&
+        req.body.state.submission &&
+        req.body.state.submission.result &&
+        req.body.state.submission.result.passed
+
+    if (!provider.outcome_service) {
+      res.send({error:'must be a student'})
+      return
+    }
+    console.log('collected vars')
+    if (correct) {
+      function cb(err, result) {
+        console.log('grade submission result',err,result)
+        res.send({error:err,success:result})
+      }
+      provider.outcome_service.send_replace_result_with_text( 1, code, cb )
+    } else {
+      provider.outcome_service.send_replace_result_with_text( 0, code, cb )
+    }
+    
+  } else {
+    res.send({error:'session not found. try reloading'})
+  }
+})
+
+app.post('/lti', async (req, res) => {
+    /* TODO - fetch user's previous submission */
+  
+  const provider = new lti.Provider( consumer_key,
+                                     consumer_secret )
+  console.log('lti launch params',req.body)
+  provider.valid_request(req, (err, isValid) => {
+    if (err) {
+      console.error('invalid request',err)
+      res.send(err + ". check your consumer key and consumer secret.")
+    } else {
+      console.log('provider good',provider)
+      // if external tool is an assignment, then it will have outcome_service_url
+      var assignment
+      if (req.query.assignmentid) {
+        // fetch the assignment id information
+        assignment = getAssignment(req.query.assignmentid)
+      }
+      var sessid = Math.floor(Math.random() * 1000000).toString()
+      cheapsession[sessid] = {req, provider, assignment}
+      const tdata = {
+        body:req.body,
+        assignment:assignment,
+        provider:provider,
+        session: sessid
+      }
+      ejs.renderFile('views/lti.html', tdata, null, (err,str) => {
+        res.send(str)
+      })
+    }
+  })
+})
+const port = 3030
+app.listen(port, function () {
+  console.log(`ltitool on ${port}`)
+})
---
+++ canvas-coding-quiz-lti/public/lti.js
@@ -0,0 +1,139 @@
+function runTestIframe(text, challenge) {
+  return new Promise( res => {
+    var iframe = document.createElement('iframe')
+    iframe.src = 'testframe.html'
+    iframe.style.display='none'
+    document.querySelector('.test-iframe').innerHTML = ''
+    document.querySelector('.test-iframe').appendChild(iframe)
+    iframe.contentWindow.addEventListener('message', function(e) {
+      if (e.data.loaded) {
+        iframe.contentWindow.postMessage({command:'runTest',text:text,challenge:challenge},'*')
+      } else {
+        if (e.data.command) { return }
+        console.log('iframe returns message',e.data)
+        res(e.data)
+      }
+    }, false)
+  })
+}
+
+
+async function submit_code(user_code, assignment) {
+  // client side only checking, in a web worker
+  var result = await runTestIframe(user_code, assignment)
+  console.log('submit code final result',result)
+  return result
+}
+
+async function submit_solution(user_code, state, assignment) {
+  const API_ENDPOINT = '/lti-grade'
+  const body = {
+    code: user_code,
+    state: state,
+    assignment: assignment,
+    session: sessid
+  }
+  const opts = {
+    method:'post',
+    headers: { "Content-Type": "application/json; charset=utf-8" },
+    body: JSON.stringify(body)
+  }
+  const result = await fetch(API_ENDPOINT, opts)
+  const j = await result.json()
+  console.log('submit solution result',result,j)
+  return j
+}
+
+async function submit_code_server(user_code, test_user_code) {
+  // cheat-free version
+  const API_ENDPOINT = '/api/grade'
+  const body = {
+    code:user_code,
+    tests:test_user_code,
+    debugLevel:0
+  }
+  const opts = {
+    method:'post',
+    headers: { "Content-Type": "application/json; charset=utf-8" },
+    body: JSON.stringify(body)
+  }
+  const result = await fetch(API_ENDPOINT, opts)
+  const j = await result.json()
+  console.log('api result',result,j)
+  return j
+}
+
+class CodeComponent extends React.Component {
+  async onSubmit() {
+    console.log('submit code to grader')
+    var result = await submit_solution(this._editor.getValue(), this.state, assignment)
+    if (result.success) {
+      this.setState({completed:true})
+    }
+  }
+  async onCheck() {
+    console.log('submit/check',this)
+    this.setState({submitting:true})
+    const user_code = this._editor.getValue()
+    const test_user_code = assignment.tests
+    const sub_result = await submit_code(user_code, assignment)
+    this.setState({submitting:false, submission:sub_result})
+  }
+  onChange() {
+  }
+  constructor() {
+    super()
+  }
+  componentDidMount() {
+    var t = ''
+    for (let line of assignment.challengeSeed) {
+       t += line + '\n'
+    }
+    document.getElementById('editor').textContent = t
+    var editor = ace.edit("editor");
+    this._editor = editor
+    //editor.setTheme("ace/theme/twilight");
+    editor.session.setMode("ace/mode/javascript");
+  }
+  render() {
+    var msg = ''
+    var submit = ''
+    if (this.state && this.state.completed) {
+      return (<div>All done with this assignment!</div>)
+    }
+    
+    if (this.state && this.state.submission && this.state.submission.result) {
+      if (this.state.submission.result.message) msg = this.state.submission.result.message
+      if (this.state.submission.result.passed) {
+        submit = (<input type="button" defaultValue="Submit Solution" onClick={this.onSubmit.bind(this)} />)
+        msg = 'Great! Your code passed all tests.'
+      }
+    }
+    
+    return  (
+        <div>
+        <h1>Code assignment</h1>
+        <h3>{assignment.title}</h3>
+        <div dangerouslySetInnerHTML={{__html:assignment.description}}></div>
+        <pre id="editor">
+        
+      </pre>
+        <span dangerouslySetInnerHTML={{__html:msg}}></span>
+<br />
+        <input type="button" defaultValue="Check Code"
+      onClick={this.onCheck.bind(this)}
+        />
+      { submit }
+        <pre style={{display:'none'}}>
+        {JSON.stringify(this.state,null,2)}
+      </pre>
+        </div>);
+  }
+}
+
+
+ReactDOM.render(
+    <CodeComponent />,
+  document.getElementById('root')
+);
+
