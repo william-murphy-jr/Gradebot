30af4685afd88d879b09446fa977cc8d4bb856ef
Michael Christenson <mchristenson5891@gmail.com>
2018-02-15 09:29:13 -0800

    add style to gradebot

--- canvas-coding-quiz-lti/config.js
+++ canvas-coding-quiz-lti/config.js
@@ -1,4 +1,4 @@
-exports.canvas_api_token = '3awo3we8UGnKCuI1EzwXP61BbVPrYZUG6DYsYv1WLydDweGXa629Mc0uFrxSrYXG'
+exports.canvas_api_token = 
 exports.canvas_base = 'http://canvas-dev.tlmworks.org'
 exports.lti_root = 'http://127.0.0.1:3030'
 exports.consumer_key = 'gradebot' // your_app_name e.g. gradebot
