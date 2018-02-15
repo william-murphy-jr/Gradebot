bf5a58985c7f907eed7e68d53b2426eedbe36dd4
Michael Christenson <mchristenson5891@gmail.com>
2018-02-15 09:40:22 -0800

    start ignoring config.js

--- canvas-coding-quiz-lti/config.js
+++
@@ -1,5 +0,0 @@
-exports.canvas_api_token = 
-exports.canvas_base = 'http://canvas-dev.tlmworks.org'
-exports.lti_root = 'http://127.0.0.1:3030'
-exports.consumer_key = 'gradebot' // your_app_name e.g. gradebot
-exports.consumer_secret = 'consumersecret' // a secret string
