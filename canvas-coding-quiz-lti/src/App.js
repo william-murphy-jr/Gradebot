34f92c267f20679b524a65a8ab11f783071cde83
Michael Christenson <mchristenson5891@gmail.com>
2018-02-22 09:12:19 -0800

    create new app component

--- canvas-coding-quiz-lti/src/App.js
+++ canvas-coding-quiz-lti/src/App.js
@@ -5,7 +5,6 @@ import React, { Component } from 'react';
 import { assert } from 'chai';
 import AceEditor from 'react-ace';
 import axios from 'axios'
-import runTest from './testframe.js'
 window.assert = assert
 
 import 'brace/mode/javascript';
@@ -14,9 +13,95 @@ import 'brace/theme/monokai';
 class App extends Component {
 
   state = {
-    assignment: [],
-    description: [],
-    challengeSeed: [],
+    assignment:  {
+      "id": "56533eb9ac21ba0edf2244d8",
+      "title": "Comparisons with the Logical And Operator",
+      "description": [
+        "Sometimes you will need to test more than one thing at a time. The <dfn>logical and</dfn> operator (<code>&&</code>) returns <code>true</code> if and only if the <dfn>operands</dfn> to the left and right of it are true.",
+        "The same effect could be achieved by nesting an if statement inside another if:",
+        "<blockquote>if (num > 5) {<br>  if (num < 10) {<br>    return \"Yes\";<br>  }<br>}<br>return \"No\";</blockquote>",
+        "will only return \"Yes\" if <code>num</code> is greater than <code>5</code> and less than <code>10</code>. The same logic can be written as:",
+        "<blockquote>if (num > 5 && num < 10) {<br>  return \"Yes\";<br>}<br>return \"No\";</blockquote>",
+        "<hr>",
+        "Combine the two if statements into one statement which will return <code>\"Yes\"</code> if <code>val</code> is less than or equal to <code>50</code> and greater than or equal to <code>25</code>. Otherwise, will return <code>\"No\"</code>."
+      ],
+      "releasedOn": "January 1, 2016",
+      "challengeSeed": [
+        "function testLogicalAnd(val) {",
+        "  // Only change code below this line",
+        "",
+        "  if (val) {",
+        "    if (val) {",
+        "      return \"Yes\";",
+        "    }",
+        "  }",
+        "",
+        "  // Only change code above this line",
+        "  return \"No\";",
+        "}",
+        "",
+        "// Change this value to test",
+        "testLogicalAnd(10);"
+      ],
+      "solutions": [
+        "function testLogicalAnd(val) {\n  if (val >= 25 && val <= 50) {\n    return \"Yes\";\n  }\n  return \"No\";\n}"
+      ],
+      "tests": [
+        "assert(code.match(/&&/g).length === 1, 'message: You should use the <code>&&</code> operator once');",
+        "assert(code.match(/if/g).length === 1, 'message: You should only have one <code>if</code> statement');",
+        "assert(testLogicalAnd(0) === \"No\", 'message: <code>testLogicalAnd(0)</code> should return \"No\"');",
+        "assert(testLogicalAnd(24) === \"No\", 'message: <code>testLogicalAnd(24)</code> should return \"No\"');",
+        "assert(testLogicalAnd(25) === \"Yes\", 'message: <code>testLogicalAnd(25)</code> should return \"Yes\"');",
+        "assert(testLogicalAnd(30) === \"Yes\", 'message: <code>testLogicalAnd(30)</code> should return \"Yes\"');",
+        "assert(testLogicalAnd(50) === \"Yes\", 'message: <code>testLogicalAnd(50)</code> should return \"Yes\"');",
+        "assert(testLogicalAnd(51) === \"No\", 'message: <code>testLogicalAnd(51)</code> should return \"No\"');",
+        "assert(testLogicalAnd(75) === \"No\", 'message: <code>testLogicalAnd(75)</code> should return \"No\"');",
+        "assert(testLogicalAnd(80) === \"No\", 'message: <code>testLogicalAnd(80)</code> should return \"No\"');"
+      ],
+      "type": "waypoint",
+      "challengeType": 1,
+      "translations": {
+        "es": {
+          "title": "La comparación con el operador lógico y",
+          "description": [
+            "A veces necesitarás probar más de una cosa a la vez. El operador <dfn>lógico y</dfn> (<code>&&</code>) retorna <code>true</code>(verdadero) si y solo si los <dfn>operandos</dfn> a la izquierda y derecha de este son verdaderos.",
+            "El mismo efecto podría lograrse anidando una sentencia if dentro de otro if:",
+            "<blockquote>if (num > 5) {<br>  if (num < 10) {<br>    return \"Yes\";<br>  }<br>}<br>return \"No\";</blockquote>",
+            "solo retornará \"Yes\" si <code>num</code> está entre <code>6</code> y <code>9</code> (6 y 9 incluidos). La misma lógica puede ser escrita como:",
+            "<blockquote>if (num > 5 && num < 10) {<br>  return \"Yes\";<br>}<br>return \"No\";</blockquote>",
+            "<h4>Instrucciones</h4>",
+            "Combina las dos sentencias if dentro de una sentencia la cual retornará <code>\"Yes\"</code> si <code>val</code> es menor o igual a <code>50</code> y mayor o igual a <code>25</code>. De otra manera, retornará <code>\"No\"</code>."
+          ]
+        }
+      }
+    },
+      
+    description: [
+      "Sometimes you will need to test more than one thing at a time. The <dfn>logical and</dfn> operator (<code>&&</code>) returns <code>true</code> if and only if the <dfn>operands</dfn> to the left and right of it are true.",
+      "The same effect could be achieved by nesting an if statement inside another if:",
+      "<blockquote>if (num > 5) {<br>  if (num < 10) {<br>    return \"Yes\";<br>  }<br>}<br>return \"No\";</blockquote>",
+      "will only return \"Yes\" if <code>num</code> is greater than <code>5</code> and less than <code>10</code>. The same logic can be written as:",
+      "<blockquote>if (num > 5 && num < 10) {<br>  return \"Yes\";<br>}<br>return \"No\";</blockquote>",
+      "<hr>",
+      "Combine the two if statements into one statement which will return <code>\"Yes\"</code> if <code>val</code> is less than or equal to <code>50</code> and greater than or equal to <code>25</code>. Otherwise, will return <code>\"No\"</code>."
+    ],
+    challengeSeed: [
+      "function testLogicalAnd(val) {",
+      "  // Only change code below this line",
+      "",
+      "  if (val) {",
+      "    if (val) {",
+      "      return \"Yes\";",
+      "    }",
+      "  }",
+      "",
+      "  // Only change code above this line",
+      "  return \"No\";",
+      "}",
+      "",
+      "// Change this value to test",
+      "testLogicalAnd(10);"
+    ], 
     errorMsg: "",
     passed: false
   }
@@ -27,11 +112,9 @@ class App extends Component {
     axios.get('/lti')
       .then(res => {
         this.challengeSeed = res.data.initstate.assignment.challengeSeed
-        const assignment = res.data.initstate.assignment;
-        const description = res.data.initstate.assignment.description
         this.setState({
-          assignment, 
-          description, 
+          assignment: res.data.initstate.assignment,
+          description: res.data.initstate.assignment.description,
           challengeSeed: this.challengeSeed 
         });
       })
@@ -45,16 +128,15 @@ class App extends Component {
   }
 
   onCheck = () => {
-    var p1
     var code = this._editor.getValue()
-
-    // eval()
     let passed = true
     let errorMsg;
-    // eval(this.state.assignment.tail.join('\n'))
+    let head = this.state.assignment.head ? this.state.assignment.head.join('\n') : ""
+    let tail = this.state.assignment.tail ? this.state.assignment.tail.join('\n') : ""
     this.state.assignment.tests.forEach((test) => {
       try {
-        eval(code + test)
+        let codeAndTest = `${head} \n ${code} \n ${tail} \n ${test} `
+        eval(codeAndTest)
       } catch(e) {
         errorMsg = e.message.replace('message:', '')
         passed = false
