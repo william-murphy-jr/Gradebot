ef086bbcbf89cc39cb0ff852aed5801611f54f55
Michael Christenson <mchristenson5891@gmail.com>
2018-04-20 15:49:04 -0700

    add euler-problems

--- canvas-coding-quiz-lti/controllers/indexCtrl.js
+++ canvas-coding-quiz-lti/controllers/indexCtrl.js
@@ -5,7 +5,7 @@ const config = require('../config')
 const fs =require('fs')
 const path = require('path')
 const cheapsession = {}
-const fcc = load_freecodecamp_challenges()
+// const fcc = load_freecodecamp_challenges()
 const assert = require('assert')
 
 // Helper Functions
@@ -28,26 +28,26 @@ let codeEval = (req, res, next) => {
   return evalOfTests;
 }
 
-function getAssignment(id) {
-  if (fcc.fcc_index[id]) {
-    const challenge = fcc.fcc_index[id]
+// function getAssignment(id) {
+//   if (fcc.fcc_index[id]) {
+//     const challenge = fcc.fcc_index[id]
     // console.log('found FCC challenge',challenge)
-    return challenge
-  }
-  console.error(`unable to find assignment with id ${id}`)
-}
+//     return challenge
+//   }
+//   console.error(`unable to find assignment with id ${id}`)
+// }
 
-function load_freecodecamp_challenges() {
-  const fcc_includes = [ 'freeCodeCamp/seed/challenges/02-javascript-algorithms-and-data-structures/basic-javascript.json', 'freeCodeCamp/seed/challenges/08-coding-interview-questions-and-take-home-assignments/project-euler-problems.json' ]
-  const fcc_index = {}
-  fcc_includes.forEach(c => {
-    const fcc_data = JSON.parse(fs.readFileSync(c))   
-    for (let challenge of fcc_data.challenges) {
-      fcc_index[challenge.id] = challenge
-    }
-  })
-  return {fcc_index}
-}
+// function load_freecodecamp_challenges() {
+//   const fcc_includes = [ 'freeCodeCamp/seed/challenges/02-javascript-algorithms-and-data-structures/basic-javascript.json', 'freeCodeCamp/seed/challenges/08-coding-interview-questions-and-take-home-assignments/project-euler-problems.json' ]
+//   const fcc_index = {}
+//   fcc_includes.forEach(c => {
+//     const fcc_data = JSON.parse(fs.readFileSync(c))   
+//     for (let challenge of fcc_data.challenges) {
+//       fcc_index[challenge.id] = challenge
+//     }
+//   })
+//   return {fcc_index}
+// }
 
 function get(req, res) {
   // req.session.assignment = req.session.assignment || getAssignment('5900f3881000cf542c50fe9b')
