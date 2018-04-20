2eeafdd68cd6a136ffa8a0ba8ac74990a0c95752
Michael Christenson <mchristenson5891@gmail.com>
2018-04-20 15:29:26 -0700

    add euler-problems

--- canvas-coding-quiz-lti/controllers/indexCtrl.js
+++ canvas-coding-quiz-lti/controllers/indexCtrl.js
@@ -6,7 +6,6 @@ const fs =require('fs')
 const path = require('path')
 const cheapsession = {}
 const fcc = load_freecodecamp_challenges()
-console.log(fcc)
 const assert = require('assert')
 
 // Helper Functions
@@ -52,6 +51,7 @@ function load_freecodecamp_challenges() {
 
 function get(req, res) {
   req.session.assignment = req.session.assignment || getAssignment('5900f36e1000cf542c50fe80')
+  console.log(req.parmas)
   res.send({assignment : req.session.assignment, sessionId: req.session.sessionId })
 }
 
