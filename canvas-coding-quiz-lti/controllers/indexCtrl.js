aa70f49e6448e4bca494d94304695b67b51e92e5
Michael Christenson <mchristenson5891@gmail.com>
2018-05-07 15:44:36 -0700

    add HTML challenges

--- canvas-coding-quiz-lti/controllers/indexCtrl.js
+++ canvas-coding-quiz-lti/controllers/indexCtrl.js
@@ -60,7 +60,7 @@ function get(req, res) {
   const assignment = getAssignment('bad87fee1348bd9aedf08801')
   assignment.syntax = req.session.syntax || "html"
 
-  console.log(assignment)
+  console.log(assignment.syntax)
   res.send({assignment: req.session.assignment || assignment, sessionId: req.session.sessionId})
 }
 
