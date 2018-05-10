4f7f0165d779aeeb92913cae8b6b2dda55643658
Michael Christenson <mchristenson5891@gmail.com>
2018-05-10 10:46:21 -0700

    add assignement and id to route in ltiCtrl

--- canvas-coding-quiz-lti/routes/index.js
+++ canvas-coding-quiz-lti/routes/index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const router = express.Router()
 const indexCtrl = require('../controllers/indexCtrl.js')
 
-router.get('/:assignmentId/:sessionId', indexCtrl.getInit)
+// router.get('/:assignmentId/:sessionId', indexCtrl.getInit)
 router.get('/get-state', indexCtrl.getState)
 router.post('/check-answer', indexCtrl.check)
 
