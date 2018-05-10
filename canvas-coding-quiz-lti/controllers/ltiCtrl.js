9afef9ef2c0344b02dae94afd43e4550d0a8e186
Michael Christenson <mchristenson5891@gmail.com>
2018-05-10 11:38:07 -0700

    add assignement and id to route in ltiCtrl

--- canvas-coding-quiz-lti/controllers/ltiCtrl.js
+++ canvas-coding-quiz-lti/controllers/ltiCtrl.js
@@ -109,6 +109,7 @@ async function submit(req, res) {
 
 function get (req,res) {
   console.log(cheapsession)
+  console.log(req.session)
   // console.log(req.session.cheapsession[req.params.sessionId])
   res.sendFile(path.resolve(`${__dirname}/../client/build/index.html`))
 }
