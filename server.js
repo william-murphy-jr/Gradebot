08d9676a2b9d57f68ea914f409c1b7c7119b94e9
Michael Christenson <mchristenson5891@gmail.com>
2019-10-21 08:12:39 -0700

    remove check for env

--- server.js
+++ server.js
@@ -25,12 +25,10 @@ app.use(session({
 app.use('/', indexRoute)
 app.use('/lti', ltiRoute)
 
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')))
-  app.get('*', function (req, res) {
-    res.sendFile(path.join(__dirname, 'build', 'index.html'))
-  })
-}
+app.use(express.static(path.join(__dirname, 'build')))
+app.get('*', function (req, res) {
+  res.sendFile(path.join(__dirname, 'build', 'index.html'))
+})
 
 app.listen(PORT, err => console.log(err || `server running on ${PORT}`))
 
