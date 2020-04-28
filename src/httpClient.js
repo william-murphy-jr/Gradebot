7944fca047adfba29461fff05b8d0693631af9fc
Michael Christenson <mchristenson5891@gmail.com>
2020-04-28 15:36:16 -0700

    fix httpClient

--- src/httpClient.js
+++ src/httpClient.js
@@ -2,11 +2,15 @@ import axios from 'axios';
 
 const httpClient = axios.create();
 
-httpClient.getChallenge = (sessionId = '56533eb9ac21ba0edf2244a9') =>
-  this({ method: 'get', url: `/lti/getstate/${sessionId}` });
+httpClient.getChallenge = function(
+  sessionId = '56533eb9ac21ba0edf2244a9',
+) {
+  return this({ method: 'get', url: `/lti/getstate/${sessionId}` });
+};
 
-httpClient.testCode = data =>
-  this({ method: 'post', url: `/lti/checkanswer`, data });
+httpClient.testCode = function(data) {
+  return this({ method: 'post', url: `/lti/checkanswer`, data });
+};
 
 httpClient.grade = function(body, sessionId) {
   return this({
