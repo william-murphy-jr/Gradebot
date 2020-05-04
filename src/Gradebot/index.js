d85fb6bdb567920f46d903f31eae974c3e011703
Bill Murphy <bill559m@gmail.com>
2020-05-03 22:14:54 -0700

    added styles template to external style js file.

--- src/Gradebot/index.js
+++ src/Gradebot/index.js
@@ -13,6 +13,8 @@ import React, { Component } from 'react';
 import httpClient from '../httpClient.js';
 import iPhone from './iphone.png';
 
+import playGroundCSS from './playGroundStyles';
+
 import 'brace/mode/javascript';
 import 'brace/mode/html';
 import 'brace/theme/monokai';
@@ -327,71 +329,11 @@ function addBootstrap() {
 // Move either all helpers to their own file of at least the CSS template
 function addJQueryPlayGroundStyles() {
   const playGroundStyles = document.createElement('style');
-  const cssPlayGroundStyles = `
-    .well {
-      width: 84px;
-      padding-left: 2px;
-    }
-    .col-xs-6 {
-      padding-left: 11px;
-      padding-right: 11px;
-    }
-    h4 {
-      height: 40px;
-    }
-    button:not(:first-of-type) {
-      margin-top: 3px;
-    }
-
-    .animated.shake {
-      /* Start the shake animation and make the animation last for 0.5 seconds */
-      animation: shake 0.75s;
-    
-      /* When the animation is finished, start again */
-      animation-iteration-count: infinite;
-    }
-    
-    @keyframes shake {
-      0% { transform: translate(1px, 1px) rotate(0deg); }
-      10% { transform: translate(-1px, -2px) rotate(-1deg); }
-      20% { transform: translate(-3px, 0px) rotate(1deg); }
-      30% { transform: translate(3px, 2px) rotate(0deg); }
-      40% { transform: translate(1px, -1px) rotate(1deg); }
-      50% { transform: translate(-1px, 2px) rotate(-1deg); }
-      60% { transform: translate(-3px, 1px) rotate(0deg); }
-      70% { transform: translate(3px, 1px) rotate(-1deg); }
-      80% { transform: translate(-1px, -1px) rotate(1deg); }
-      90% { transform: translate(1px, 2px) rotate(0deg); }
-      100% { transform: translate(1px, -2px) rotate(-1deg); }
-    }
-
-    .animated.bounce {
-      /* Start the shake animation and make the animation last for 0.5 seconds */
-      animation: bounce 1.5s;
-    
-      /* When the animation is finished, start again */
-      animation-iteration-count: infinite;
-    }
-    
-    @keyframes bounce {
-      0% { transform: translate( 0px, 0px); }
-      10% { transform: translate(0px, 3px); }
-      20% { transform: translate(0px, 6px); }
-      30% { transform: translate(0px, 9px); }
-      40% { transform: translate(0px, 6px); }
-      50% { transform: translate(0px, 3px); }
-      60% { transform: translate(0px, 0px); }
-      70% { transform: translate(0px, -3px); }
-      80% { transform: translate(0px,-6px); }
-      87% { transform: translate(0px, -9px); }
-      94% { transform: translate(0px, -6px); }
-      100% { transform: translate(0px, -3px); }
-    }
-  `;
+  console.log(playGroundStyles)
 
   const head = document.getElementById('iframe').contentWindow
     .document.head;
-  playGroundStyles.innerHTML = cssPlayGroundStyles;
+  playGroundStyles.innerHTML = playGroundCSS;
   head.append(playGroundStyles);
 }
 
